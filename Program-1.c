991b418fede0d561a69ff133bbbe968fdf798319
vaishk21 <kotavaish21@gmail.com>
2021-03-25 09:58:41 +0000

    done

--- Program-1.c
+++ Program-1.c
@@ -1,19 +1,11 @@
-Complete the given program with string declaration/initialization & read/print the string
-// Instructions:-
-//1) Initially, declare a string with size
-//2) read the input string from user 
-//3) Finally, Print the input string obtained from user.
 
-//Expected input & output:-
-//Enter a String: Vidyavardhaka
-//The input string is Vidyavardhaka
 
 #include <stdio.h>
 int main() 
 {
-                      //Declare the string
-printf("Enter a String: ")
-                      //read the string from user
-                      //Print the string
+ char str[20];                     //Declare the string
+printf("Enter a String: ");
+scanf("%s",str);
+printf("the entered string is %s",str);
 return 0;
 }
